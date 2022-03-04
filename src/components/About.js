d504660922abe2f89ee670e89edf3edd6f99d349
Ragillio Aji <lioajiragil@gmail.com>
2022-03-04 21:29:54 +0700

    change about

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
                         Hi, I'm Ragillio Aji.
                     </h1>
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        I am (mediocre) Web Developer.
+                        I am (mediocre) Software Engineer.
                     </h1>
                     <p className="leading-relaxed">
                         Simple Boy. Gamer by Day, Coder by Night.
