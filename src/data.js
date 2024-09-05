4c509fb630326e827fc1d89c1c1548ed7ff5a0ce
ragillio <lioajiragil@gmail.com>
2024-09-05 22:13:10 +0700

    add new projects

--- src/data.js
+++ src/data.js
@@ -1,48 +1,58 @@
 const projects = [
-    {
-        title: "Yournotes App",
-        subtitle: "MERN Stack",
-        description: "Built with M.E.R.N. stack (Mongo DB, Express js, React, Node js). YourNotes is a React App. In this app you can add, edit, and delete notes (ordinary CRUD app). There also has an authentication and authorization method.",
-        image: "./react_proj.gif",
-        link: "https://github.com/ragil132/yournotes-fe",
-    },
-    {
-        title: "OpenMusic API",
-        subtitle: "HapiJS + PostgreSQL",
-        description: "Built with Hapi Js, PostgreSQL, Redis, RabbitMQ. A RESTful API which you can do some CRUD operations on music stuff, like adding a song, look some playlists, etc. I built this after i've learnt it from Dicoding",
-        image: "./backend_proj.gif",
-        link: "https://github.com/ragil132/Ragillio-OpenMusic-API",
-    },
-    {
-        title: "Book Express API",
-        subtitle: "ExpressJS + MySQL",
-        description:
-            "Simple RESTful API which can do CRUD things like adding book, get all books, update book data, delete book.",
-        image: "./express.gif",
-        link: "https://github.com/ragil132/book-express-mysql-api",
-    },
-    {
-        title: "Villa Reservation System for Griya Asri",
-        subtitle: "Laravel + MySQL",
-        description: "Built with Laravel 7 (frontend & backend) and MySQL/MariaDB. This project is a part of my final test for achieving my Bachelor's Degree at University.",
-        image: "./laravel_proj.gif",
-        link: "#",
-    },
+  {
+    title: "Yournotes App",
+    subtitle: "MERN Stack",
+    description:
+      "Built with M.E.R.N. stack (Mongo DB, Express js, React, Node js). YourNotes is a React App. In this app you can add, edit, and delete notes (ordinary CRUD app). There also has an authentication and authorization method.",
+    image: "./react_proj.gif",
+    link: "https://github.com/ragil132/yournotes-fe",
+  },
+  {
+    title: "OpenMusic API",
+    subtitle: "HapiJS + PostgreSQL",
+    description:
+      "Built with Hapi Js, PostgreSQL, Redis, RabbitMQ. A RESTful API which you can do some CRUD operations on music stuff, like adding a song, look some playlists, etc. I built this after i've learnt it from Dicoding",
+    image: "./backend_proj.gif",
+    link: "https://github.com/ragil132/Ragillio-OpenMusic-API",
+  },
+  {
+    title: "Internal E-meterai Application",
+    subtitle: "React, Golang, Docker",
+    description:
+      "An internal application used in Panin Dubai Syariah Bank for stamping a document with e-meterai supplied by Peruri.",
+    image: "./emeterai.png",
+    link: "",
+  },
+  {
+    title: "Villa Reservation System for Griya Asri",
+    subtitle: "Laravel + MySQL",
+    description:
+      "Built with Laravel 7 (frontend & backend) and MySQL/MariaDB. This project is a part of my final test for achieving my Bachelor's Degree at University.",
+    image: "./laravel_proj.gif",
+    link: "#",
+  },
 ];
 
 const skills = [
-    'NodeJS', 'PHP', 'MySQL', 'MS SQL Server', 'Codeigniter', 'React', 'HapiJS', 'Laravel'
+  "NodeJS",
+  "PHP",
+  "All SQL Based Database",
+  "Python",
+  "Codeigniter",
+  "React",
+  "Golang",
+  "Laravel",
 ];
 
 const jobs = [
-    {
-        name: 'Panin Dubai Syariah Bank',
-        duration: 'April 2022-Now',
-        role: 'IT Developer EUC Officer',
-        imageUrl: './pdsb.jpg',
-        isCurrent: true,
-        lastSeenDateTime: '2023-01-23T13:23Z'
-    }
-]
+  {
+    name: "Panin Dubai Syariah Bank",
+    duration: "April 2022-Now",
+    role: "IT Developer",
+    imageUrl: "./pdsb.jpg",
+    isCurrent: true,
+    lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+];
 
-export { projects, skills, jobs }
+export { projects, skills, jobs };
